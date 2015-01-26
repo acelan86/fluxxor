72290191ad64b13158e02cd42c9045f30fcd609d
晓斌 蓝 <acelan@xiaobindeMacBook-Pro.local>
2015-01-27 01:04:07 +0800

    简化业务代码的书写
    将getStateFromStore作为默认mixin方法，从flux.stores中自动获取state，但需要在Store的方法中增加getS
    tate用于调用

--- build/fluxxor.js
+++ build/fluxxor.js
@@ -437,39 +437,72 @@ return /******/ (function(modules) { // webpackBootstrap
 	var _each = __webpack_require__(22);
 	
 	var StoreWatchMixin = function() {
-	  var storeNames = Array.prototype.slice.call(arguments);
+	  var storeNames;
 	  return {
 	    componentDidMount: function() {
 	      var flux = this.props.flux || this.context.flux;
+	      var storeNames = this._getStoreNames();
 	      _each(storeNames, function(store) {
 	        flux.store(store).on("change", this._setStateFromFlux);
 	      }, this);
 	    },
-	
+
 	    componentWillUnmount: function() {
 	      var flux = this.props.flux || this.context.flux;
+	      var storeNames = this._getStoreNames();
 	      _each(storeNames, function(store) {
 	        flux.store(store).removeListener("change", this._setStateFromFlux);
 	      }, this);
 	    },
-	
+
+	    getStateFromFlux: function () {
+	      var flux = this.props.flux || this.contex.flux;
+	      var state = {};
+	      var storeNames = this._getStoreNames();
+	      _each(storeNames, function (store) {
+	        if ('function' !== typeof flux.store(store).getState) {
+	          throw new Error("Store [" + store + "] must have getState function to return state for view");
+	        } else {
+	          state[store] = flux.store(store).getState();
+	        }
+	      });
+	      return state;
+	    },
+
 	    _setStateFromFlux: function() {
 	      if(this.isMounted()) {
 	        this.setState(this.getStateFromFlux());
 	      }
 	    },
-	
+
+	    _getStoreNames: function () {
+	      var stores,
+	          names;
+	      if (!storeNames) {
+	        stores = (this.props.flux || this.context.flux).stores;
+	        names = [];
+	        for (var name in stores) {
+	          if (stores.hasOwnProperty(name)) {
+	            names.push(name);
+	          }
+	        }
+	        storeNames = names;
+	      }
+	      return storeNames;
+	    },
+
 	    getInitialState: function() {
 	      return this.getStateFromFlux();
 	    }
 	  };
 	};
-	
+
 	StoreWatchMixin.componentWillMount = function() {
 	  throw new Error("Fluxxor.StoreWatchMixin is a function that takes one or more " +
 	    "store names as parameters and returns the mixin, e.g.: " +
 	    "mixins[Fluxxor.StoreWatchMixin(\"Store1\", \"Store2\")]");
 	};
+
 	
 	module.exports = StoreWatchMixin;
 
